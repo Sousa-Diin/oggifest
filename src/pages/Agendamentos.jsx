80d3ce929761b09e4aaa3fd3a3d0cdd110805942
Wildes Sousa <augustowildes@gmail.com>
2025-03-26 10:11:50 -0300

    :rocket: create style table agendamento

--- src/pages/Agendamentos.jsx
+++ src/pages/Agendamentos.jsx
@@ -1,45 +1,61 @@
 import { useNavigate } from 'react-router-dom';
 import { IoMdArrowBack } from "react-icons/io";
+import { RiDeleteBin6Line } from "react-icons/ri";
+import { MdOutlineModeEditOutline } from "react-icons/md";
 import { getStoredEvents } from '../provider/AuthContextProvider.jsx';
   
   const Agendamentos = ({openPage, setOpenPage}) => {
 
     const appointments  = getStoredEvents();
-    const listCabecalhoSheet = ['Data','Horário','Nome', 'Quantidade','Valor','Pedido','Situação'];
+    const listCabecalhoSheet = ['Id', 'Data','Horário','Nome', 'Quantidade','Valor','Pedido','Situação', 'Ações'];
     const count = 0;
     const handleChangePage = () => {
       setOpenPage(!openPage);
     };
 
     return (
-      <div className='flex-col'>
-        <aside className='flex gap-3 p-2'>
+      <div className='flex-col lg:w-full md:dvw items-center'>
+        <div className='flex gap-3 p-2'>
           <button onClick={handleChangePage}><IoMdArrowBack/></button>
-          <h2>Lista de Agendamentos</h2>
-        </aside> 
-        <header className="flex bg-[#963584]">
-          {listCabecalhoSheet.map( (title) => ( <tr className="w-46">{title}</tr>))}
-        </header>
-        <div className=' flex-col h-96 p-1 justify-between '>
-        {appointments.length === 0 ? (
-          <p>Nenhum agendamento encontrado.</p>
-        ) : (
-          appointments.map((appointment) => (
-            
-            <div key={appointment.id} className="flex  ">
-                   {/* <div className="w-2 h-8 bg-yellow-500 mr-2"></div> */}
-                  
-                   <span className="w-46">{appointment.Saida}</span>
-                   <span className="w-46">{appointment.Horario}</span>
-                   <span className="w-55 font-bold">{appointment.Cliente}</span>
-                   <span className="w-40">{appointment.Quantidade}</span>
-                   <span className="w-46">{appointment.Valor}</span>
-                   <span className="w-46">{appointment.Pedido}</span>
-                   <span className="w-46">{appointment.Status} {appointment.Status === "Pago" ? '✔' : appointment.status === "Entrada" ? '⚠' : '❌'}</span>
-            </div>
-          ))
-        )}
-        </div>
+          <p>Lista de Agendamentos</p>
+        </div> 
+        <table className='flex-col lg:w-full md:min-w-full'>
+          <tr className="flex lg:justify-around lg:w-full md:w-dvw md:justify-between  p-1 bg-[#E59E07]">
+            {listCabecalhoSheet.map( (title) => ( <td className="">{title}</td>))}
+          </tr>
+          <tr className='h-96 overflow-y-auto'>
+            <div className=' flex-col p-1 justify-center '>
+              {appointments.length === 0 ? (
+                <p>Nenhum agendamento encontrado.</p>
+              ) : (
+                appointments.map((appointment) => (
+
+                  <tr key={appointment.id} className="flex lg:w-full  justify-between lg:mb-1  lg:h-8 items-center text-zinc-700 ">
+                         {/* <div className="w-2 h-8 bg-yellow-500 mr-2"></div> */}
+
+                         <td className="bg-[#fcfcfc] lg:w-full md:w-full p-1 shadow ">{appointment.Id}</td>
+                         <td className="bg-[#fcfcfc] lg:w-full md:w-full p-1 shadow  ">{appointment.Saida}</td>
+                         <td className="bg-[#fcfcfc] lg:w-full md:w-full p-1 shadow ">{appointment.Horario}</td>
+                         <td className="bg-[#fcfcfc] lg:w-full md:w-full p-1 shadow  font-bold">{appointment.Cliente}</td>
+                         <td className="bg-[#fcfcfc] lg:w-full md:w-full p-1 shadow ">{appointment.Quantidade}</td>
+                         <td className="bg-[#fcfcfc] lg:w-full md:w-full p-1 shadow ">{appointment.Valor}</td>
+                         <td className="bg-[#fcfcfc] lg:w-full md:w-full p-1 shadow ">{appointment.Pedido}</td>
+                         <td className="bg-[#fcfcfc] lg:w-full md:w-full p-1 shadow ">{appointment.Status} {appointment.Status === "Pago" ? '✔' : appointment.status === "Entrada" ? '⚠' : '❌'}</td>
+                         <div className=' flex '>
+                            <button className='lg:w-13 p-2  bg-[#37A2C2] cursor-pointer'><MdOutlineModeEditOutline /></button>
+                            <button className='lg:w-13 p-2  bg-[#37A2C2] cursor-pointer'><RiDeleteBin6Line /></button>
+                         </div>
+                  </tr>
+                ))
+              )}
+              </div>
+          </tr>
+         
+
+        </table>
+       
+
+        
         
       </div>
     );
--- src/pages/main/Store.jsx
+++ src/pages/main/Store.jsx
@@ -10,9 +10,9 @@ function Store() {
   const [openPage, setOpenPage] = useState(true); //muda de pagina
 
   return (
-    <main className='flex w-full min-h-dvh bg-[#EAE8E1]'>
+    <main className='flex lg:w-full md:min-w-dvh  min-h-dvh bg-[#EAE8E1]'>
       <SideBar openPage={openPage} setOpenPage={setOpenPage}  />
-      <div className='aside-content ml-10.5 md:ml-14 overflow-y-auto'>
+      <div className='aside-content lg:ml-10.5 md:ml-14 '>
         {openPage ? 
           <OggiFest/> :
           <Agendamentos openPage={openPage} setOpenPage={setOpenPage}/>

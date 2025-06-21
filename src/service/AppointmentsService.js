32b2718f46d4a7ea9ca7e23d7e721907271731b1
Wildes Sousa <augustowildes@gmail.com>
2025-06-21 20:42:37 -0300

    feature(service) -> connect-route-appointment, :gear: Fixes #19, ppointment route

---
+++ src/service/AppointmentsService.js
@@ -0,0 +1,21 @@
+import { googleScriptClient } from "../api/googleScriptClient.js";
+
+export const getAllAppointments = () => googleScriptClient('appointments', 'GET');
+
+export const getAppointmentByCod = (cod) => googleScriptClient('appointments', 'GET', { cod });
+
+export const createAppointment = (dados) => googleScriptClient('appointments', 'POST', {
+  method: 'CREATE',
+  ...dados
+});
+
+
+export const updateAppointment = (dados) => googleScriptClient('appointments', 'POST', {
+  method: 'UPDATE',
+  ...dados
+});
+
+export const deleteAppointment = (cod) => googleScriptClient('appointments', 'POST', {
+  method: 'DELETE',
+  cod
+});
